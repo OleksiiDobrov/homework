f620dc6ca940fda7786c13bd954e08e8701917dd
OleksiiDobrov <82.aalexx.82@gmail.com>
2020-05-17 00:50:49 +0300

    addDZ8

--- less8dz/script.js
+++ less8dz/script.js
@@ -2,20 +2,11 @@ const input = document.querySelector('.addCase');
 const addBtn = document.querySelector('#addBtn');
 const list = document.querySelector('.toDoList');
 
-console.log(input.value);
+addBtn.addEventListener('click', onAddBtnClick);
 
 function onAddBtnClick(){
     const li = document.createElement('li');
     li.textContent = input.value;
     list.append(li);
     input.value = '';
-
-console.log(li);
 }
-
-
-addBtn.addEventListener('click', onAddBtnClick);
-addBtn.addEventListener('click', onAddBtnClick);
-
-console.log(addBtn);
-console.log(list);
